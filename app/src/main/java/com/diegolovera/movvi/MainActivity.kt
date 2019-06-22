fdbf7e139a958ce4bc86267203dce231dce6e6d9
DiegoLovera <diegolovera010@gmail.com>
2019-06-22 12:46:36 -0500

    Initial commit

---
+++ app/src/main/java/com/diegolovera/movvi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.diegolovera.movvi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
