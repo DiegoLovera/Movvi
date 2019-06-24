47d8535e4ea41dedff02502dff704d6168a414d3
Diego Lovera <diegolovera010@gmail.com>
2019-06-24 10:30:34 -0500

    final accent color

--- app/src/androidTest/java/com/diegolovera/movvi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/diegolovera/movvi/ExampleInstrumentedTest.kt
@@ -1,24 +1,40 @@
 package com.diegolovera.movvi
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.intent.Intents
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.diegolovera.movvi.ui.activities.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.FixMethodOrder
+import org.junit.Rule
+import org.junit.runners.MethodSorters
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+    }
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.diegolovera.movvi", appContext.packageName)
+        // Not enough time to implement them :(
     }
 }
